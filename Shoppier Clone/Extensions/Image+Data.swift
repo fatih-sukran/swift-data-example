0827cf54a57bcd6590a2639ff9e12c2214ddb51c
Fatih Şükran <fth.21.81@gmail.com>
2023-10-02 21:47:18 +0300

    fonts and Image extension

---
+++ Shoppier Clone/Extensions/Image+Data.swift
@@ -0,0 +1,8 @@
+//
+//  Image+Data.swift
+//  Shoppier Clone
+//
+//  Created by fatih.sukran on 30.09.2023.
+//
+
+import Foundation
---
+++ Shoppier Clone/Helper/SelectImageView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectImageView.swift
+//  Shoppier Clone
+//
+//  Created by fatih.sukran on 30.09.2023.
+//
+
+import SwiftUI
+
+struct SelectImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectImageView()
+}
